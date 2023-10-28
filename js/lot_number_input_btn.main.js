8c9ac5f519f801b2f35ce74018e6dfd330622fc9
PronayBormon <pronaykumarbormon@gmail.com>
2023-10-28 16:03:40 +0600

    notification

--- js/lot_number_input_btn.main.js
+++ js/lot_number_input_btn.main.js
@@ -1,36 +1,37 @@
-$(".button").click(function() {
+// $(".button").click(function() {
     
 
-    var buttonText = $(this).text();
+//     var buttonText = $(this).text();
     
-    // Check if any input field contains the same value
-    var $targetInput = $(".input").filter(function() {
-        return $(this).val() === buttonText;
-    });
+//     // Check if any input field contains the same value
+//     var $targetInput = $(".input").filter(function() {
+//         return $(this).val() === buttonText;
+//     });
 
-    if ($targetInput.length > 0) {
-        // If input field contains the same value, remove it
+//     if ($targetInput.length > 0) {
+//         // If input field contains the same value, remove it
        
-        $(this).removeClass("active");
-        $targetInput.val("");
-    }else if($targetInput.length > 0 ){
-        window.alert("full");
-    }
-     else {
-        // Find the next empty input field and set its value
-        $(".input").filter(function() {
-            return $(this).val() === "";
-        }).first().val(buttonText);
-        $(this).addClass("active");
-        // if($(".clicks.active").length == 5) {
-        //     $(this).removeClass("active");
-        //     $("#message").fadeIn();
-        // }else{
-        // $(this).addClass("active");
-        // }
-    }
+//         $(this).removeClass("active");
+//         $targetInput.val("");
+//     }else if($targetInput.length > 0 ){
+//         window.alert("full");
+//     }
+//      else {
+//         // Find the next empty input field and set its value
+//         $(".input").filter(function() {
+//             return $(this).val() === "";
+//         }).first().val(buttonText);
+//         $(this).addClass("active");
+//         if($(".clicks.active").length == 5) {
+//             $(this).removeClass("active");
+//             $("#message").fadeIn();
+//         }else if($(".clicks.active").length < 5){
+//             $(this).addClass("active");
+//             $("#message").fadeOut();
+//         }
+//     }
     
-});
+// });
 
 
 $(".btn_reset").click(function(){
